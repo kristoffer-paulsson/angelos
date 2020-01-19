c528f7231565a6f2535ddc3cfedbfcdc9bd0cb53
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2020-01-19 22:44:49 +0100

    .

--- lib/libangelos/task/network_index.pyx
+++ lib/libangelos/task/network_index.pyx
@@ -99,4 +99,4 @@ class NetworkIndexerTask(TaskFacadeExtension):
         writer.writerows(self.__network_list)
         self.__network_list = None
 
-        await self.facade.storage.vailt.save_settings("networks.csv", csv_data.getvalue().encode())
+        await vault.save_settings("networks.csv", csv_data.getvalue().encode())
